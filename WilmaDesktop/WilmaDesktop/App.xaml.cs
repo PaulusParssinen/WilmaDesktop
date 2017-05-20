909e8ca14f4411238acf9850449c54584b2e8468
PaulusParssinen <08paulus.parssinen@edupori.fi>
2017-05-20 16:49:52 +0300

    Started using PRISM's Bootstrapper in initialization of the application.

--- WilmaDesktop/WilmaDesktop/App.xaml.cs
+++ WilmaDesktop/WilmaDesktop/App.xaml.cs
@@ -8,10 +8,14 @@ using System.Windows;
 
 namespace WilmaDesktop
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Bootstrapper bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
---
+++ WilmaDesktop/WilmaDesktop/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+using Prism.Regions;
+using Prism.Unity;
+
+using Microsoft.Practices.Unity;
+using WilmaDesktop.Constants;
+using WilmaDesktop.Views;
+
+namespace WilmaDesktop
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+            => Container.Resolve<MainWindow>();
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            
+            var regionManager = Container.Resolve<IRegionManager>();
+
+            regionManager?.RegisterViewWithRegion(
+                ContentRegion.MainWindowRight, typeof(SelectServerView));
+
+            Container.RegisterTypeForNavigation<LoginView>(NavigationView.LoginView);
+
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
