873fe64da549525961f42836048bb6234a94254b
PaulusParssinen <08paulus.parssinen@edupori.fi>
2017-05-20 16:48:03 +0300

    Moved Login stuff to own views.

---
+++ WilmaDesktop/WilmaDesktop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Mvvm;
+using Prism.Regions;
+using WilmAPI.Json;
+
+namespace WilmaDesktop.ViewModels
+{
+    public class LoginViewModel : BindableBase, INavigationAware
+    {
+        private WilmaServer _server;
+        public WilmaServer Server {
+            get { return _server; }
+            set
+            {
+                _server = value;
+
+                RaisePropertyChanged(nameof(Server));
+            }
+        }
+
+        public LoginViewModel()
+        {
+            
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            Server = navigationContext
+                .Parameters["server"] as WilmaServer;
+        }
+
+        #region NavigationInterface
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        { return true; }
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        { }
+#endregion
+    }
+}
