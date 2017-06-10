7f35ae5b13d5e9152889b971a7e85c6c933ad667
PaulusParssinen <08paulus.parssinen@edupori.fi>
2017-06-10 22:16:53 +0300

    Added converter for the login parameters.

---
+++ WilmaDesktop/WilmaDesktop/Converters/LoginParameterConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace WilmaDesktop.Converters
+{
+    public class LoginParameterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
